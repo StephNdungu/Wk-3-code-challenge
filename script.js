e3643108672332895942a23e4fc960c499c7631d
StephNdungu <stephy.wanjiku@gmail.com>
2024-07-16 19:14:25 +0300

    Adds fetch functions

--- script.js
+++ script.js
@@ -1,21 +1,43 @@
 const list = document.getElementById("films")
 const image = document.getElementById('movieDesc')
-fetch ("http://localhost:3000/films")
+const buyTicketButton = document.querySelector('.buyTicketButton')
+buyTicketButton.addEventListener('click', () => {
+  //initiates payment process
+
+
+})
+
+function fetchData() {
+  fetch ("http://localhost:3000/films")
 .then(response => response.json())
 .then(films => 
   films.map(film => {
     const movieTitles = document.createElement('li')
     movieTitles.innerText = film.title
     list.appendChild(movieTitles)
-    const title2 = document.createElement('h1')
+   
+  }))
+}
+fetchData()
+
+function fetchFirstMovie() {
+  fetch ("http://localhost:3000/films/1")
+.then(response => response.json())
+.then(film => {
+
+ const title2 = document.createElement('h1')
     title2.innerText = film.title
     image.appendChild(title2)
     const images = document.createElement('img')
     images.src = film.poster
     image.appendChild(images)
-
-    const title3 = document.createElement('h1')
-    title3.innertext = film.title
-    image.appendChild(title3)
-  
-  }))
+    const runTime = document.createElement('p')
+    runTime.innerText = film.runtime
+    image.appendChild(runTime)
+    const showTime = document.createElement('p')
+    showTime.innerHTML = film.showtime 
+    image.appendChild(showTime)
+}
+)
+}
+fetchFirstMovie()
